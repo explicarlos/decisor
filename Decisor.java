2ce45500117979938179611208b2999f9498ac70
Carlos Grasa <explicarlos@gmail.com>
2024-05-13 14:28:02 +0200

    Add files via upload

--- Decisor.java
+++ Decisor.java
@@ -8,124 +8,124 @@
  */
 public class Decisor {
 
-	// campos de clase ---------------------------------------------------------
-	Object estadoInicial; // estado inicial a evaluar
-	private int nivelMax=0; // máximo nivel de profundidad permitido (0=infinito)
-	private int nivelObjetivo; // nivel buscado en fase actual
-	private int nivelRecord; // registro de mayor nivel alcanzado
-	private long lapsoPermitido=0L; // tiempo permitido de cálculo en ms (0=infinito)
-	private long instanteRegistrado; // registro de tiempo en ms
-	private boolean esProfundidadProgresiva=true; // es o no búsqueda en anchura
-	private double cotaInferior=Double.MIN_VALUE; // valor alfa para poda
-	private double cotaSuperior=Double.MAX_VALUE; // valor beta para poda
-	private int mejorOpcion=0; // mejor opcion encontrada
-	private double mejorPuntuacion; // puntuación de la mejor opción encontrada
-	private boolean esJugadaConRetroceso=true; // se deshacen o no las jugadas en estado global único
+    // campos de clase ---------------------------------------------------------
+    Object estadoInicial; // estado inicial a evaluar
+    private int nivelMax=0; // máximo nivel de profundidad permitido (0=infinito)
+    private int nivelObjetivo; // nivel buscado en fase actual
+    private int nivelRecord; // registro de mayor nivel alcanzado
+    private long lapsoPermitido=0L; // tiempo permitido de cálculo en ms (0=infinito)
+    private long instanteRegistrado; // registro de tiempo en ms
+    private boolean esProfundidadProgresiva=true; // es o no búsqueda en anchura
+    private double cotaInferior=Double.MIN_VALUE; // valor alfa para poda
+    private double cotaSuperior=Double.MAX_VALUE; // valor beta para poda
+    private int mejorOpcion=0; // mejor opcion encontrada
+    private double mejorPuntuacion; // puntuación de la mejor opción encontrada
+    private boolean esJugadaConRetroceso=true; // se deshacen o no las jugadas en estado global único
 
-	// constructores ----------------------------------------------------------------------------
-	public Decisor() { // constructor por omisión
-		return;
-	}
-	public Decisor(Object estadoInicial, long lapsoPermitido, int nivelMax, boolean esProfundidadProgresiva) { // constructor básico
-		if (nivelMax>=0)
-			this.nivelMax = nivelMax;
-		if (lapsoPermitido>=0)
-			this.lapsoPermitido = lapsoPermitido;
-		this.esProfundidadProgresiva = esProfundidadProgresiva;
-		this.estadoInicial=estadoInicial;
-		return;
-	}
+    // constructores ----------------------------------------------------------------------------
+    public Decisor() { // constructor por omisión
+        return;
+    }
+    public Decisor(Object estadoInicial, long lapsoPermitido, int nivelMax, boolean esProfundidadProgresiva) { // constructor básico
+        if (nivelMax>=0)
+            this.nivelMax = nivelMax;
+        if (lapsoPermitido>=0)
+            this.lapsoPermitido = lapsoPermitido;
+        this.esProfundidadProgresiva = esProfundidadProgresiva;
+        this.estadoInicial=estadoInicial;
+        return;
+    }
 
-	// main -------------------------------------------------------------------------------------
-	public static void main(String[] args) { // método estático principal
+    // main -------------------------------------------------------------------------------------
+    public static void main(String[] args) { // método estático principal
 
-		return;
-	}
+        return;
+    }
 
-	// getters y setters ---------------------------------------------------------------------------
-	public int getNivelMax() {
-		return nivelMax;
-	}
-	public void setNivelMax(int nivel) {
-		nivelMax=nivel;
-		return;
-	}
-	public int getNivelObjetivo() {
-		return nivelObjetivo;
-	}
-	public void setNivelObjetivo(int nivel) {
-		nivelObjetivo=nivel;
-		return;
-	}
-	public int getNivelRegistrado() {
-		return nivelRecord;
-	}
-	public void setNivelRegistrado(int nivel) {
-		nivelRecord = nivel;
-	}
-	public long getLapsoPermitido() {
-		return lapsoPermitido;
-	}
-	public void setLapsoPermitido(long lapso) {
-		lapsoPermitido = lapso;
-		return;
-	}
-	public boolean getProfundidadProgresiva() {
-		return esProfundidadProgresiva;
-	}
-	public void setProfundidadProgresiva(boolean esBusquedaAnchura) {
-		esProfundidadProgresiva = esBusquedaAnchura;
-		return;
-	}
-	public boolean getJugadaConRetroceso() {
-		return esJugadaConRetroceso;
-	}
-	public void setJugadaConRetroceso(boolean hayRetroceso) {
-		esJugadaConRetroceso = hayRetroceso;
-		return;
-	}
+    // getters y setters ---------------------------------------------------------------------------
+    public int getNivelMax() {
+        return nivelMax;
+    }
+    public void setNivelMax(int nivel) {
+        nivelMax=nivel;
+        return;
+    }
+    public int getNivelObjetivo() {
+        return nivelObjetivo;
+    }
+    public void setNivelObjetivo(int nivel) {
+        nivelObjetivo=nivel;
+        return;
+    }
+    public int getNivelRegistrado() {
+        return nivelRecord;
+    }
+    public void setNivelRegistrado(int nivel) {
+        nivelRecord = nivel;
+    }
+    public long getLapsoPermitido() {
+        return lapsoPermitido;
+    }
+    public void setLapsoPermitido(long lapso) {
+        lapsoPermitido = lapso;
+        return;
+    }
+    public boolean getProfundidadProgresiva() {
+        return esProfundidadProgresiva;
+    }
+    public void setProfundidadProgresiva(boolean esBusquedaAnchura) {
+        esProfundidadProgresiva = esBusquedaAnchura;
+        return;
+    }
+    public boolean getJugadaConRetroceso() {
+        return esJugadaConRetroceso;
+    }
+    public void setJugadaConRetroceso(boolean hayRetroceso) {
+        esJugadaConRetroceso = hayRetroceso;
+        return;
+    }
 
-	// métodos de clase -----------------------------------------------------------------------
+    // métodos de clase -----------------------------------------------------------------------
 
-	/**
-	 * Realiza un corte aleatorio en la "baraja" de ramas a explorar secuencialmente
-	 * @param numRamas int con el número de ramas a explorar
-	 * @return int con el índice de rama a explorar primero
-	 */
-	public int sortearRamaInicial(int numRamas) { // decidir aleatoriamente en cuál rama se inicia búsqueda
-		return (int) Math.floor(numRamas*Math.random());
-	}
+    /**
+     * Realiza un corte aleatorio en la "baraja" de ramas a explorar secuencialmente
+     * @param numRamas int con el número de ramas a explorar
+     * @return int con el índice de rama a explorar primero
+     */
+    public int sortearRamaInicial(int numRamas) { // decidir aleatoriamente en cuál rama se inicia búsqueda
+        return (int) Math.floor(numRamas*Math.random());
+    }
 
-	/**
-	 * Actualiza el registro de tiempo y calcula la diferencia con el anterior en ms
-	 * @return long diferencia de tiempo entre los dos últimos registros en ms
-	 */
-	public long actualizarInstante() { // actualiza registro de tiempo en ms y devuelve la diferencia de tiempo en ms
-		long instante=instanteRegistrado;
-		instanteRegistrado=System.currentTimeMillis();
-		return instanteRegistrado-instante;
-	}
+    /**
+     * Actualiza el registro de tiempo y calcula la diferencia con el anterior en ms
+     * @return long diferencia de tiempo entre los dos últimos registros en ms
+     */
+    public long actualizarInstante() { // actualiza registro de tiempo en ms y devuelve la diferencia de tiempo en ms
+        long instante=instanteRegistrado;
+        instanteRegistrado=System.currentTimeMillis();
+        return instanteRegistrado-instante;
+    }
 
-	/**
-	 * Calcula el número de ramas posibles desde el nodo (estado) actual
-	 * @param estado objeto con la posición del tablero de juego
-	 * @return
-	 */
-	public int getNumOpciones(Object estado) { // calcula el número de opciones disponibles para el estado actual
+    /**
+     * Calcula el número de ramas posibles desde el nodo (estado) actual
+     * @param estado objeto con la posición del tablero de juego
+     * @return
+     */
+    public int getNumOpciones(Object estado) { // calcula el número de opciones disponibles para el estado actual
 
-		return 0;
-	}
+        return 0;
+    }
 
-	/**
-	 *
-	 * @param estado objeto del estado del tablero
-	 * @param jugada int última jugada realizada
-	 * @param nivel int nivel actual de profundidad
-	 * @return int valoración del nodo explorado
-	 */
-	public double evaluarNodo(Object estado, int jugada, int nivel) { // realiza la evaluación del nodo a partir de un estado, última jugada realizada y nivel de profundidad
+    /**
+     *
+     * @param estado objeto del estado del tablero
+     * @param jugada int última jugada realizada
+     * @param nivel int nivel actual de profundidad
+     * @return int valoración del nodo explorado
+     */
+    public double evaluarNodo(Object estado, int jugada, int nivel) { // realiza la evaluación del nodo a partir de un estado, última jugada realizada y nivel de profundidad
 
-		return 0.0;
-	}
+        return 0.0;
+    }
 
 }
